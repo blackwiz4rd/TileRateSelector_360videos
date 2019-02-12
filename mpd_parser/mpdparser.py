1e232dae2f0671666cc844f9877e9270fae2b67b
luca <luca@lucas-MBP-2.lan>
2019-02-13 00:22:22 +0100

    fixed mpd_parser qt, qt_variations

--- mpd_parser/mpdparser.py
+++ mpd_parser/mpdparser.py
@@ -8,8 +8,6 @@ def calculate_action(x, bitrate):
 	# print(bitrate[bitrate.size-1], range(bitrate.size-2))
 	if x >= bitrate[0]: # if bandwidth is higher than 5800 set 5800
 		return 0
-	elif x <= bitrate[6]:
-	 	return 6
 	elif x < bitrate[5]:
 	 	return 6
 	for i in range(bitrate.size-2):
@@ -46,18 +44,23 @@ data = data.reshape(n_segments,8*2)
 df = pd.DataFrame(data)
 df = df.drop(np.arange(1,17,2), axis=1) # drop exceeding cols
 # FINISHED PARSING MPD
+df = pd.concat([df]*38).reset_index(drop=True)
+print(df.shape)
 
 bandwidth = np.load('../bandwidth_extractor/bandwidth.npy')/1e3 # retreiving bandwidth
+bandwidth = np.tile(bandwidth,38)
+print(bandwidth.shape)
 prob = np.load('../tile_prediction_nn/prob.npy') # estimated nn probability
 true_prob = np.load('../tile_prediction_nn/y_validation.npy') # true probability
-print(true_prob)
+# print(true_prob)
 print(prob.shape, true_prob.shape)
 # adding columns to dataframe
 df['bandwidth'] = bandwidth # in kbit/s
-df['prob'] = prob.reshape(prob.shape[0]*prob.shape[1])[4200:4200+bandwidth.size]
-df['true'] = true_prob.reshape(prob.shape[0]*prob.shape[1])[4200:4200+bandwidth.size]
+df['prob'] = prob.reshape(prob.shape[0]*prob.shape[1])[:bandwidth.size]
+df['true'] = true_prob.reshape(prob.shape[0]*prob.shape[1])[:bandwidth.size]
 
-df.drop(df.tail(11).index,inplace=True) # drop last 11 rows
+df = df.iloc[:-2] # drop last two rows
+
 temp = np.add.reduceat(df['bandwidth'], range(0, df['bandwidth'].size, 16))
 temp_bandwidth16 = temp
 df['bandwidth16'] = np.repeat(temp,16)
@@ -72,6 +75,7 @@ df['perfect_bandw'] = df['bandwidth16']*df['true']/np.repeat(temp,16)
 bitrate =  np.array([5800, 4300, 3750, 3000, 2350, 1750, 110]) # 7, from 0 to 6
 action = np.array([])
 perfect_action = np.array([])
+df = df.apply(pd.to_numeric) # convert data to numeric
 # print(df['bandw'].size)
 for i in range(df['bandw'].size):
 	x = df['bandw'][i]
@@ -89,7 +93,6 @@ df['action'] = action
 
 df.columns = ['action','perfect_bandw', 'bandw', 'bandwidth16', 'true', 'prob', 'bandwidth', '750k', '5800k', '4300k', '3750k', '3000k', '2350k', '1750k', '110k'][::-1]
 # 375k -> 3750k
-df = df.apply(pd.to_numeric) # convert data to numeric
 
 # calculate maximum achieved quality
 achieved_quality = np.array([])
@@ -116,8 +119,11 @@ print(df.iloc[:,:7])
 
 fig1, ax1 = plt.subplots()
 # in viewport
-ax1.boxplot([achieved_quality*df["true"], best_quality*df["true"]])
+achieved_quality = np.array(achieved_quality*df["true"])[np.nonzero(achieved_quality*df["true"])]
+best_quality = np.array(best_quality*df["true"])[np.nonzero(best_quality*df["true"])]
+ax1.boxplot([achieved_quality, best_quality])
 plt.xticks([1, 2], ["Achieved quality", "Best quality achievable"])
+plt.ylabel('Quality (SSIM)')
 plt.show()
 
 fig1, ax1 = plt.subplots()
@@ -126,14 +132,8 @@ ax1.boxplot([achieved_delta_quality, best_delta_quality])
 print(np.sum(np.abs(achieved_delta_quality)))
 print(np.sum(np.abs(best_delta_quality)))
 plt.xticks([1, 2], ["Achieved delta quality in viewport","Best delta quality in viewport"])
+plt.ylabel('Delta quality (SSIM)')
 plt.show()
-
-# plt.imshow(true_prob[200:300,:].T,interpolation='none',aspect='auto')
-# plt.xlabel('i-th tile')
-# plt.ylabel('j-th segment')
-# plt.colorbar()
-# plt.yticks(np.arange(1,16))
-# plt.show()
 
 ## plot capacity in kbit/s from given bit/s
 ax = plt.figure().gca()
